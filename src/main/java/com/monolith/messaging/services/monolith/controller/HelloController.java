ce0bd7b5d11c06a1730c5ebe1e478d182fbcd652
tkmoney11 <tylerkim@outlook.com>
2021-06-09 00:15:29 -0700

    Hello Controller

---
+++ src/main/java/com/monolith/messaging/services/monolith/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.monolith.messaging.services.monolith.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("hello")
+public class HelloController {
+    @GetMapping("")
+    public String hello() {
+        System.out.println("This is hello");
+        return "Hello, Monolith";
+    }
+}
