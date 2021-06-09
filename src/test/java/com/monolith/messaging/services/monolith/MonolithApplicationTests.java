c6d6f93a672407fb15719d559ebf519945a7b894
tkmoney11 <tylerkim@outlook.com>
2021-06-09 00:03:08 -0700

    first

---
+++ src/test/java/com/monolith/messaging/services/monolith/MonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monolith.messaging.services.monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
