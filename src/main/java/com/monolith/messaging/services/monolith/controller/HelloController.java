8aebdda359d9d75e7933bd6537231dd5779df521
tkmoney11 <tylerkim@outlook.com>
2021-06-13 00:04:13 -0700

    CORS

--- src/main/java/com/monolith/messaging/services/monolith/controller/HelloController.java
+++ src/main/java/com/monolith/messaging/services/monolith/controller/HelloController.java
@@ -3,7 +3,7 @@ package com.monolith.messaging.services.monolith.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("hello")
 public class HelloController {
     @GetMapping("")
--- src/main/java/com/monolith/messaging/services/monolith/controller/MessageController.java
+++ src/main/java/com/monolith/messaging/services/monolith/controller/MessageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("message")
 public class MessageController {
     @Autowired
